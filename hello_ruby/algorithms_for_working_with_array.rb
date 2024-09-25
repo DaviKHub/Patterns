33f7d7dc47eb33f8f9ed70ddb7fe87940855d16e
DaviKHub <175750928+DaviKHub@users.noreply.github.com>
2024-09-25 17:17:28 +0300

    Lab1 Task 5.3
    Реализовал метод, находящий номер первого положительного элемента.

--- hello_ruby/algorithms_for_working_with_array.rb
+++ hello_ruby/algorithms_for_working_with_array.rb
@@ -18,9 +18,21 @@ def find_max_element(array)
   return max
 end
 
+def first_plus(array)
+  first_element=0
+  for i in 0..array.length-1
+    if array[i]>0
+      first_element = i
+      break
+    end
+  end
+  return first_element+1
+end
+
 
 print "Полученный массив: "
 array = [-1, 5, 141, -914, 351, 1, 0, -9]
 print(array, " \n")
 print "Минимальный элемент: #{find_min_element(array)}\n"
 print "Максимальный элемент: #{find_max_element(array)}\n"
+print"Номер первого положительного элемента: #{first_plus(array)}\n"
