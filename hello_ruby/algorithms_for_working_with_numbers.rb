1a237d2efcdd1b3967beb55c226dc5737bd9f7de
DaviKHub <175750928+DaviKHub@users.noreply.github.com>
2024-09-25 14:41:03 +0300

    Lab1 Task 4.2
    Реализовал метод, находящий минимальную нечетную цифру числа.

--- hello_ruby/algorithms_for_working_with_numbers.rb
+++ hello_ruby/algorithms_for_working_with_numbers.rb
@@ -10,6 +10,17 @@ def count_div_non_div_by_3(n)
   return count
 end
 
+def min_odd_digit(n)
+  digits = []
+  while n > 0
+    digits.append(n % 10) if (n % 10) % 2 != 0
+    n /= 10
+  end
+  return digits.min || nil
+end
+
+
 print "Введите число: "
 n = gets.to_i.abs # to_i - преобразование строки в целое десятичное
 print "Количество делителей, которые не кратны 3: #{count_div_non_div_by_3(n)}\n"
+print "Минимальная нечетная цифра: #{min_odd_digit(n)}\n"
