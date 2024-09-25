9b5c5c7e704e252142871019faace7f6ddf3e9b0
DaviKHub <175750928+DaviKHub@users.noreply.github.com>
2024-09-25 15:21:12 +0300

    Lab1 Task5.2
    Реализовал метод, находящий максимальный элемент.

--- hello_ruby/algorithms_for_working_with_array.rb
+++ hello_ruby/algorithms_for_working_with_array.rb
@@ -8,8 +8,19 @@ def find_min_element(array)
   return min
 end
 
+def find_max_element(array)
+  max=array[0]
+  for i in 1..array.length-1
+    if array[i] > max
+      max = array[i]
+    end
+  end
+  return max
+end
+
 
 print "Полученный массив: "
 array = [-1, 5, 141, -914, 351, 1, 0, -9]
 print(array, " \n")
 print "Минимальный элемент: #{find_min_element(array)}\n"
+print "Максимальный элемент: #{find_max_element(array)}\n"
