819c9cae3302ab55eba72aabbab662f3d195c251
DaviKHub <175750928+DaviKHub@users.noreply.github.com>
2024-10-02 17:52:26 +0300

    Lab2 Task2
    Переопределил метод to_s в классе Student для удобства вывода. Создал объект класса Student в отдельном файле main.rb и в этом же файле вывел информацию об объекте класса. Исправил опечатку в коде student.rb

---
+++ classes_core_models/main.rb
@@ -0,0 +1,8 @@
+require_relative 'student'
+student = Student.new(surname: "Иванов",
+                      first_name: "Иван",
+                      second_name: "Иванович",
+                      phone: "123-456-7890",
+                      telegram: "@ivan")
+
+print student
--- classes_core_models/student.rb
+++ classes_core_models/student.rb
@@ -10,56 +10,69 @@ class Student
     @mail=mail if mail!=nil
     @git=git if git!=nil
   end
-end
 
 #написание  геттеров
-def id
-  @id
-end
-def surname
-  @surname
-end
-def first_name
-  @first_name
-end
-def second_name
-  @second_name
-end
-def phone
-  @phone
-end
-def telegram
-  @telegram
-end
-def mail
-  @mail
-end
-def git
-  @git
-end
+  def id
+    @id
+  end
+  def surname
+    @surname
+  end
+  def first_name
+    @first_name
+  end
+  def second_name
+    @second_name
+  end
+  def phone
+    @phone
+  end
+  def telegram
+    @telegram
+  end
+  def mail
+    @mail
+  end
+  def git
+    @git
+  end
 
 #написание  сеттеров
-def id=(value)
-  @id = value
-end
-def surname=(value)
-  @surname = value
-end
-def first_name=(value)
-  @first_name = value
-end
-def second_name=(value)
-  @second_name = value
-end
-def phone=(value)
-  @phone = value
-end
-def telegram=(value)
-  @telegram = value
-end
-def mail=(value)
-  @mail = value
-end
-def git=(value)
-  @git = value
+  def id=(value)
+    @id = value
+  end
+  def surname=(value)
+    @surname = value
+  end
+  def first_name=(value)
+    @first_name = value
+  end
+  def second_name=(value)
+    @second_name = value
+  end
+  def phone=(value)
+    @phone = value
+  end
+  def telegram=(value)
+    @telegram = value
+  end
+  def mail=(value)
+    @mail = value
+  end
+  def git=(value)
+    @git = value
+  end
+
+  def to_s
+    str = []
+    str.push ("ID: #{@id}" ) if id
+    str.push("Фамилия: #{@surname}")
+    str.push("Имя: #{@first_name}")
+    str.push("Отчество: #{@second_name}")
+    str.push("Телефон: #{@phone}") if phone
+    str.push("Телеграм: #{@telegram}" ) if telegram
+    str.push("Почта: #{@mail}" )if mail
+    str.push("GitHub: #{@git}") if git
+    return str.join("\n")
+  end
 end
