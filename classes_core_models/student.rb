f995462d2e5b19c24beb088a7bf171b58211acf6
DaviKHub <175750928+DaviKHub@users.noreply.github.com>
2024-10-04 23:41:05 +0300

    Lab2 Task4,5
    Воспользовался атрибутами для краткого написания геттеров и сеттеров

--- classes_core_models/student.rb
+++ classes_core_models/student.rb
@@ -1,71 +1,20 @@
 class Student
+  attr_accessor :id, :surname, :first_name, :second_name, :phone, :telegram, :mail, :git
   def initialize(id:nil, surname:, first_name:, second_name:,
                  phone:nil, telegram:nil, mail:nil, git:nil)
-    @id=id if id!=nil
+    @id=id
     @surname=surname
     @first_name=first_name
     @second_name=second_name
-    @phone=phone if phone!=nil
-    @telegram=telegram if telegram!=nil
-    @mail=mail if mail!=nil
-    @git=git if git!=nil
-  end
-
-#написание  геттеров
-  def id
-    @id
-  end
-  def surname
-    @surname
-  end
-  def first_name
-    @first_name
-  end
-  def second_name
-    @second_name
-  end
-  def phone
-    @phone
-  end
-  def telegram
-    @telegram
-  end
-  def mail
-    @mail
-  end
-  def git
-    @git
-  end
-
-#написание  сеттеров
-  def id=(value)
-    @id = value
-  end
-  def surname=(value)
-    @surname = value
-  end
-  def first_name=(value)
-    @first_name = value
-  end
-  def second_name=(value)
-    @second_name = value
-  end
-  def phone=(value)
-    @phone = value
-  end
-  def telegram=(value)
-    @telegram = value
-  end
-  def mail=(value)
-    @mail = value
-  end
-  def git=(value)
-    @git = value
+    @phone=phone
+    @telegram=telegram
+    @mail=mail
+    @git=git
   end
 
   def to_s
     str = []
-    str.push ("ID: #{@id}" ) if id
+    str.push ("ID: #{@id}") if id
     str.push("Фамилия: #{@surname}")
     str.push("Имя: #{@first_name}")
     str.push("Отчество: #{@second_name}")
@@ -73,6 +22,6 @@ class Student
     str.push("Телеграм: #{@telegram}" ) if telegram
     str.push("Почта: #{@mail}" )if mail
     str.push("GitHub: #{@git}") if git
-    return str.join("\n")
+    str.join("\n")
   end
 end
