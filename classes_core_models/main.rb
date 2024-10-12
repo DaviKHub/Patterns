333a18afa01e7667c4ee451e0da46fb82e9b24c5
DaviKHub <175750928+DaviKHub@users.noreply.github.com>
2024-10-12 03:15:23 +0300

    Lab2 Task 2.5, 2.7, 2.8, 2.9, 2.10
    Произвел рефакторинг student.rb, выделив супер-класс Person.
    Релизовал в main.rb методы read_from_txt и write_to_txt и протестировал их совместимость

--- classes_core_models/main.rb
+++ classes_core_models/main.rb
@@ -1,23 +1,33 @@
 require_relative 'student'
 
-student_info={
-  surname:"Иванов",
-  name:"Иван",
-  patronymic:"Иванович",
-  phone:"+7912345678",
-  telegram:"@ivanov",
-  mail:"ivanov@ya.ru",
+student_info = {
+  id: "1987654321",
+  surname: "Иванов",
+  name: "Иван",
+  patronymic: "Иванович",
+  phone: "+7912345678",
+  telegram: "@ivanov",
+  mail: "ivanov@ya.ru",
+  git: "https://github.com/ivanov"
 }
+string = "ID: 1111111111; Фамилия: Тестов; Имя: Тест; Отчество: Тестович; Телефон: +79123456789; Телеграм: @test; Почта: test@ya.ru; GitHub: https://github.com/test"
+student = Student.parse_from_string(string)
 
-student=Student.new(student_info)
-puts student.validate #валидация не прошла (не введен гит)
-student.set_contacts(git:"https://github.com/ivanov") #уточнение гита
-puts student.validate #валидация прошла (все данные введены)
+def read_from_txt(path)
+  students = []
+  File.readlines(path, encoding: 'UTF-8').each do |line|
+    students.push(Student.parse_from_string(line))
+  end
+  return students
+end
 
-string_student_info="ID: 1234567890; Фамилия: Тестов; Имя: Тест; Отчество: Тестович; Телефон: +79123456789; Телеграм: @test; Почта: test@ya.ru; GitHub: https://github.com/test"
-student2=Student.parse_from_string(string_student_info)
-puts student2.validate
-student_short=Student_short.new(string_student_info)
-puts student_short.validate
-student_short=Student_short.new(student2)
-puts student_short.validate
+def write_to_txt(path, file_name, students)
+  File.open("#{path}/#{file_name}.txt", "w") do |file|
+    file.write(students.join("\n"))
+  end
+end
+
+students_1 = read_from_txt('/Users/david/Patterns_proj/classes_core_models/students.txt')
+write_to_txt('/Users/david/Patterns_proj/classes_core_models/', 'students_1', students_1)
+students_2 = read_from_txt('/Users/david/Patterns_proj/classes_core_models/students_1.txt')
+puts students_1.map(&:to_s) == students_2.map(&:to_s)
--- classes_core_models/student.rb
+++ classes_core_models/student.rb
@@ -1,101 +1,42 @@
-class Student
-  attr_accessor :id
-  attr_reader :surname, :name, :patronymic, :phone, :telegram, :mail, :git # автоматическое создание только геттеров
+class Person
+  attr_accessor :name, :surname, :patronymic
+  attr_reader :phone, :telegram, :mail
+
   def initialize(params = {})
-    self.id = params[:id] if params[:id]
-    self.surname = params[:surname] if params[:surname]
     self.name = params[:name] if params[:name]
+    self.surname = params[:surname] if params[:surname]
     self.patronymic = params[:patronymic] if params[:patronymic]
-    set_contacts(params) if params.key?(:phone) || params.key?(:telegram) || params.key?(:mail) || params.key?(:git)
+    set_contact(params)
   end
 
-  def set_contacts(params = {})
+  def set_contact(params = {})
     self.phone = params[:phone] if params[:phone]
     self.telegram = params[:telegram] if params[:telegram]
     self.mail = params[:mail] if params[:mail]
-    self.git = params[:git] if params[:git]
-  end
-
-  def id=(id)
-    Student.valid_id?(id) ? @id = id : puts("ID is invalid")
   end
 
   def surname=(surname)
-    Student.valid_surname?(surname) ? @surname = surname : puts("Surname is invalid")
+    self.class.valid_surname?(surname) ? @surname = surname : raise(ArgumentError)
   end
 
   def name=(name)
-    Student.valid_name?(name) ? @name = name : puts("Name is invalid")
+    self.class.valid_name?(name) ? @name = name : raise(ArgumentError)
   end
 
   def patronymic=(patronymic)
-    Student.valid_patronymic?(patronymic) ? @patronymic = patronymic : puts("Patronymic is invalid")
+    self.class.valid_patronymic?(patronymic) ? @patronymic = patronymic : raise(ArgumentError)
   end
 
   private def phone=(phone)
-    Student.valid_phone?(phone) ? @phone = phone : puts("Phone number is invalid")
+    self.class.valid_phone?(phone) ? @phone = phone : raise(ArgumentError)
   end
+
   private def telegram=(telegram)
-    Student.valid_telegram?(telegram) ? @telegram = telegram : puts("Telegram is invalid")
+    self.class.valid_telegram?(telegram) ? @telegram = telegram : raise(ArgumentError)
   end
+
   private def mail=(mail)
-    Student.valid_mail?(mail) ? @mail = mail : puts("Mail is invalid")
-  end
-  private def git=(git)
-    Student.valid_git?(git) ? @git = git : puts("Git link is invalid")
-  end
-
-  def to_s
-    str = []
-    str.push ("ID: #{@id}") if id
-    str.push("Фамилия: #{@surname}")
-    str.push("Имя: #{@name}")
-    str.push("Отчество: #{@patronymic}")
-    str.push("Телефон: #{@phone}") if phone
-    str.push("Телеграм: #{@telegram}") if telegram
-    str.push("Почта: #{@mail}") if mail
-    str.push("GitHub: #{@git}") if git
-    str.join("\n")
-  end
-
-  def self.parse_from_string(string)
-    data = {}
-    string.split("; ").each do |pair| key, value = pair.split(": ").map(&:strip)
-      case key
-      when "ID" then data[:id] = value.to_i
-      when "Фамилия" then data[:surname] = value
-      when "Имя" then data[:name] = value
-      when "Отчество" then data[:patronymic] = value
-      when "Телефон" then data[:phone] = value
-      when "Телеграм" then data[:telegram] = value
-      when "Почта" then data[:mail] = value
-      when "GitHub" then data[:git] = value
-      end
-    end
-    self.new(**data) # преобразование в хэш с помощью **
-  end
-
-  def contact_info
-    if phone
-      "Телефон: #{@phone}"
-    elsif mail
-      "Почта: #{@email}"
-    elsif telegram
-      "Телеграм: #{@telegram}"
-    end
-  end
-
-  def get_info
-    info = "ФИО: #{@surname} #{@name[0].upcase}.#{@patronymic[0].upcase}; "
-    info += "GitHub: #{@git}; " if git
-    info += "Телефон: #{@phone}; " if phone
-    info += "Телеграм: #{@telegram}; " if telegram
-    info += "Почта: #{@mail}; " if mail
-    info.strip
-  end
-
-  def self.valid_id?(id)
-    id.to_s.match?(/\A\d{10}+\z/)
+    self.class.valid_mail?(mail) ? @mail = mail : raise(ArgumentError)
   end
 
   def self.valid_surname?(surname)
@@ -122,80 +63,145 @@ class Student
     mail.match?(/\A[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[^@\s]+\z/)
   end
 
+  def self.parse_from_string(string)
+    data = {}
+    string.split('; ').each do |pair|
+      key, value = pair.split(': ').map(&:strip)
+      case key
+      when "Фамилия"
+        data[:surname] = value
+      when "Имя"
+        data[:name] = value
+      when "Отчество"
+        data[:patronymic] = value
+      when "Телефон"
+        data[:phone] = value
+      when "Почта"
+        data[:mail] = value
+      when "Телеграм"
+        data[:telegram] = value
+      end
+    end
+    data
+  end
+end
+
+class Student < Person
+  attr_reader :id, :git
+
+  def initialize(params = {})
+    super(surname: params[:surname],
+          name: params[:name],
+          patronymic: params[:patronymic],
+          phone: params[:phone],
+          telegram: params[:telegram],
+          mail: params[:mail]
+    )
+    self.id = params[:id] if params[:id]
+    self.git = params[:git] if params[:git]
+  end
+
+  def id=(id)
+    self.class.valid_id?(id) ? @id = id : raise(ArgumentError)
+  end
+
+  def git=(git)
+    self.class.valid_git?(git) ? @git = git : raise(ArgumentError)
+  end
+
+  def self.valid_id?(id)
+    # changed to instance method
+    id.to_s.match?(/\A[0-9]{10}+\z/)
+  end
+
   def self.valid_git?(git)
+    # changed to instance method
     git.match?(/\Ahttps:\/\/github.com\/[a-zA-Z0-9_-]+\z/)
   end
 
-  def self.any_contact_present?(phone, telegram, mail)
+  private def any_contact_present?(phone, telegram, mail)
     (!phone.nil? && !phone.empty?) ||
       (!telegram.nil? && !telegram.empty?) ||
       (!mail.nil? && !mail.empty?)
   end
 
-  def self.git_present?(git)
+  private def git_present?(git)
     !git.nil? && !git.empty?
   end
 
   def validate
-    errors = []
-    errors.push("Git link is missing") if Student.git_present?(@git) == false
-    errors.push("Any contact is missing") if Student.any_contact_present?(@phone, @telegram, @mail) == false
-    return "Validating is successful" if errors.empty?
-    return "Validating is failed: #{errors.join("\n")}"
+    git_present?(@git) || any_contact_present?(@phone, @telegram, @mail)
   end
-end
 
-class Student_short < Student
-  attr_accessor :id, :git, :contact, :initials
+  def to_s
+    str = []
+    str.push ("ID: #{@id}") if id
+    str.push("Фамилия: #{@surname}")
+    str.push("Имя: #{@name}")
+    str.push("Отчество: #{@patronymic}")
+    str.push("Телефон: #{@phone}") if phone
+    str.push("Телеграм: #{@telegram}") if telegram
+    str.push("Почта: #{@mail}") if mail
+    str.push("GitHub: #{@git}") if git
+    str.compact.join("; ")
+  end
 
-  def initialize(student)
-    if student.is_a?(Student)
-      super(
-        id: student.id,
-        surname: student.surname,
-        name: student.name,
-        patronymic: student.patronymic,
-        phone: student.phone,
-        telegram: student.telegram,
-        mail: student.mail,
-        git: student.git
-      )
-      @initials = "#{student.surname} #{student.name[0].upcase}.#{student.patronymic[0].upcase}."
-      @contact = student.contact_info
-    elsif student.is_a?(String)
-      data = Student.parse_from_string(student)
-      super(
-        id: data.id,
-        surname: data.surname,
-        name: data.name,
-        patronymic: data.patronymic,
-        phone: data.phone,
-        telegram: data.telegram,
-        mail: data.mail,
-        git: data.git
-      )
-      @initials = "#{data.surname} #{data.name[0].upcase}.#{data.patronymic[0].upcase}."
-      @contact = contact_info
-    end
+  def get_info
+    info = []
+    info.push("Инициалы: #{surname}.#{name[0]}.#{patronymic[0]}.")
+    info.push("GitHub: #{@git}") if git
+    info.push("Телефон: #{@phone}") if phone
+    info.push("Телеграм: #{@telegram}") if telegram
+    info.push("Почта: #{@mail}") if mail
+
+    info.join(";")
   end
 
   def self.parse_from_string(string)
-  data = {}
-  string.split("; ").each do |pair| key, value = pair.split(": ").map(&:strip)
-    case key
-    when "ID" then data[:id] = value.to_i
-    when "ФИО" then data[:initials] = value
-    when "GitHub" then data[:git] = value
-    when "Телефон", "Почта", "Телеграм" then data[:contact] = "#{key}: #{value}"
+    data = Person.parse_from_string(string)
+    string.split('; ').each do |pair|
+      key, value = pair.split(': ').map(&:strip)
+      case key
+      when "ID"
+        data[:id] = value
+      when "GitHub"
+        data[:git] = value
+      end
     end
+    Student.new(**data)
   end
-  data
+
+
 end
 
-  def get_info
-    info = "ФИО: #{@initials}; "
-    info += "GitHub: #{@git}; " if git
-    info += "#{@contact if @contact};"
-    info.strip
+class Student_short < Student
+  attr_reader :initials
+
+  def initialize(params = {})
+    super(params)
+    @initials = "#{params[:surname]} #{params[:name][0]}.#{params[:patronymic][0]}."
   end
+
+  def to_s
+    info = []
+    info.push("ID: #{@id}") if id
+    info.push("Инициалы: #{@initials}")
+    info.push("GitHub: #{@git}") if git
+    info.compact.join("; ")
+  end
+
+  def self.parse_from_string(string)
+    data = Student.parse_from_string(string)
+    string.split('; ').each do |pair|
+      key, value = pair.split(': ').map(&:strip)
+      case key
+      when "Инициалы"
+        data[:initials] = value
+      when "GitHub"
+        data[:git] = value
+      end
+    end
+    Student_short.new(**data)
+  end
+
 end
