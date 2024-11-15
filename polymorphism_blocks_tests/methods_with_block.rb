475d21e6522544787de5ffa64efdbf28a995c77b
DaviKHub <175750928+DaviKHub@users.noreply.github.com>
2024-11-15 23:58:57 +0300

    Lab3 Task1.17
    Реализовал метод, который в массива меняет местами максимальный и минимальный элементы.

--- polymorphism_blocks_tests/methods_with_block.rb
+++ polymorphism_blocks_tests/methods_with_block.rb
@@ -2,3 +2,10 @@ def is_min?(array, index)
   raise ArgumentError if index >= array.size || index < 0
   yield(array[index] == array.min) if block_given?
 end
+
+def swap!(array)
+  min_index = array.index(array.min)
+  max_index = array.index(array.max)
+  array[min_index], array[max_index] = array[max_index], array[min_index]
+  yield(array) if block_given?
+end
