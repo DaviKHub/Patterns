85921b37452059dca1cc601f930eb3a286ff3c6b
DaviKHub <175750928+DaviKHub@users.noreply.github.com>
2024-11-21 17:05:29 +0300

    Lab4 Task1.2 get_element, count_rows, count_columns
    Реализовал методы, позволяющие – получить элемент по номеру строки и столбца без возможности его редактировать, получить количество столбцов в таблице, получить количество строк в таблице.
    Изменил модификаторы доступа.
    Дополнил валидацию

--- classes_models/data_table.rb
+++ classes_models/data_table.rb
@@ -3,11 +3,33 @@ class Data_table
     self.matrix = matrix
   end
 
-  protected def matrix=(matrix)
-    raise(ArgumentError, "Data must be a matrix") unless valid_data?(matrix)
+  private def matrix=(matrix)
+    @matrix = matrix if valid_data?(matrix)
   end
 
-  protected def valid_data?(matrix)
-    matrix.is_a?(Array) && matrix.all? { |row| row.is_a?(Array) }
+  private def valid_data?(matrix)
+    return false unless matrix.is_a?(Array) && matrix.all? { |row| row.is_a?(Array) }
+    row_length = matrix.first.length
+    return false unless matrix.all? { |row| row.length == row_length }
+    (0...row_length).all? do |col_index|
+      matrix.all? { |row| row[col_index] }
+    end
+  end
+
+  def get_element(row, column)
+    @matrix[row][column]
+  end
+
+  def count_rows
+    @matrix[0].length
+  end
+  def count_columns
+    @matrix.length
   end
 end
+
+data = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+data_table = Data_table.new(data)
+puts data_table.get_element(1, 0) # вывод 4
+puts data_table.count_rows
+puts data_table.count_columns
