d30b3a403f007c18287ff0a873ab528ee3acc125
DaviKHub <175750928+DaviKHub@users.noreply.github.com>
2024-12-20 20:16:53 +0300

    Lab4 Task1,2 Data_table class
    Реализовал класс Data_table и методы работы с ним

---
+++ student/data_table.rb
@@ -0,0 +1,47 @@
+class Data_table
+  private attr_reader :data
+
+  def initialize(data)
+    self.data = data
+  end
+
+  private def data=(data)
+    unless data.is_a?(Array) && data.all? { |row| row.is_a?(Array) }
+      raise ArgumentError, "Данные должны быть в виде двумерного массива"
+    end
+
+    def deep_dup(element)
+      if element.is_a?(Array)
+        element.map { |sub_element| deep_dup(sub_element) }
+      else
+        begin
+          element.dup
+        rescue
+          element
+        end
+      end
+    end
+
+    @data = data.map { |row| row.map { |element| deep_dup(element) } }
+  end
+
+  def get_element(num_of_row, num_of_column)
+    self.data[num_of_row][num_of_column]
+  end
+
+  def row_count
+    self.data.size
+  end
+
+  def column_count
+    if self.data.empty?
+      return 0
+    end
+    self.data[0].size
+  end
+
+  def to_s
+    self.data.inspect
+  end
+end
+
