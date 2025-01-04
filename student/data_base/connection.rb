2dae282b12ff4fe1a3e9921e8ab9ecb426e7a051
DaviKHub <175750928+DaviKHub@users.noreply.github.com>
2025-01-04 12:36:03 +0300

    Lab4 Task3.5

--- student/data_base/connection.rb
+++ student/data_base/connection.rb
@@ -1,14 +1,20 @@
 require 'pg'
 begin
-	con = PG.connect(
-		dbname: 'students',
-		user: 'postgres',
-		password: '1234',
-		host: 'localhost',
-		port: 5432
-		)
-	puts "Соединение с базой установлено"
-	con.close
+  connection = PG.connect(
+    dbname: 'students',
+    user: 'postgres',
+    password: '1234',
+    host: 'localhost',
+    port: 5432
+  )
+  puts "Соединение с базой установлено"
+  result = connection.exec("SELECT * FROM student;")
+  result.each do |row|
+    puts "ID: #{row['id']}, Фамилия: #{row['surname']}, Имя: #{row['name']}, Отчество: #{row['patronymic']}, Дата рождения: #{row['birthdate']}, Телефон: #{row['phone']}, Mail: #{row['mail']}, Telegram: #{row['telegram']}, Git: #{row['git']}"
+  end
+
 rescue PG::Error => e
-	puts "Ошибка подключения: #{e.message}"
+  puts "Ошибка подключения: #{e.message}"
+ensure
+  connection.close if connection
 end
