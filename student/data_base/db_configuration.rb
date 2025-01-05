d0827d7b429f3afb52faeec09bb8d796659639e5
DaviKHub <175750928+DaviKHub@users.noreply.github.com>
2025-01-05 13:36:22 +0300

    Lab4 Task3.11 Singleton

--- student/data_base/db_configuration.rb
+++ student/data_base/db_configuration.rb
@@ -6,14 +6,25 @@ class DBConfiguration
               password: '1234',
               host: 'localhost',
               port: 5432 }.freeze
+  @instance = nil
+
+  def self.instance
+    @instance ||= new
+  end
+
   attr_reader :connect
+
   def initialize
     @connect = PG.connect(DEFAULT)
   end
-  def execute_query(query, params=[])
+
+  def execute_query(query, params = [])
     connect.exec_params(query, params)
   end
+
   def close
     connect.close
   end
+
+  private_class_method :new
 end
--- student/main.rb
+++ student/main.rb
@@ -3,9 +3,11 @@ require_relative 'models/student_short'
 require_relative 'data_base/db_configuration'
 require_relative 'data_base/student_list_DB'
 
-db_handler=DBConfiguration.new
-student_list_db=StudentListDB.new(db_handler)
-retrieved_student = student_list_db.get_by_id(3)
-puts retrieved_student
-
-puts student_list_db.get_count
+db_handler1=DBConfiguration.instance
+result = db_handler1.execute_query("SELECT * FROM student WHERE id = $1", [3])
+result.each do |element|
+  puts element
+end
+db_handler2 = DBConfiguration.instance
+puts db_handler2.equal?(db_handler2) # true
+db_handler1.close
