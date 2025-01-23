13a5ed1da7df4f4cca1bf67a5ce07f2f40eb20c9
DaviKHub <175750928+DaviKHub@users.noreply.github.com>
2025-01-24 01:16:25 +0300

    Lab5 Task0.2-0.5 Filter, Button
    1) Добавил отображение таблицы
    2) Добавил кнопку фильтров
    3) Добавил область фильтрации
    4) Забыл сделать промежуточные коммиты

--- student/main.rb
+++ student/main.rb
@@ -1,15 +1,29 @@
-require_relative 'mvc/app'
-require 'pg'
+require_relative '../student/models/student'
+require_relative '../student/models/student_short'
+require_relative '../student/data_list/data_list'
+require_relative '../student/data_list/data_table'
+require_relative '../student/data_list/data_list_student_short'
+require_relative '../student/student_list/strategy/file_strategy_yaml'
+require_relative '../student/student_list/strategy/file_strategy_json'
+require_relative '../student/student_list/strategy/file_strategy_txt'
+require_relative '../student/student_list/strategy/file_strategy'
+require_relative '../student/student_list/student_list'
+require_relative '../student/binary_tree/tree_node'
+require_relative '../student/binary_tree/binary_tree'
+require_relative '../student/student_list/data_base/db_configuration'
+require_relative '../student/student_list/data_base/student_list_db'
+require_relative '../student/student_list/data_base/connection'
+require_relative '../student/student_list/adapter/student_db_adapter'
+require_relative '../student/student_list/adapter/student_list_adapter'
+require_relative '../student/student_list/adapter/adapter'
+require_relative '../student/mvc/app'
 require 'fox16'
 include Fox
+
 begin
-  if __FILE__ == $0
-    FXApp.new do |app|
-      StudentApp.new(app)
-      app.create
-      app.run
-    end
-  end
-rescue ArgumentError => e
-  puts e.message
+  strategy_yaml=FileStrategyYAML.new
+  list_adapter1=StudentListAdapter.new('student_test.yaml', strategy_yaml)
+  list=list_adapter1.get_k_n_student_short_list(1,10)
+  puts list.ge
+
 end
--- student/mvc/app.rb
+++ student/mvc/app.rb
@@ -1,33 +1,77 @@
 require 'fox16'
-include Fox
-class StudentApp < FXMainWindow
-  def initialize(app)
-    super(app, "Список студентов", width: 800, height: 600)
 
-    tab_book = FXTabBook.new(self, nil, 0, LAYOUT_FILL)
-    tab1 = FXTabItem.new(tab_book, "Student_list_view")
-    tab1_frame = FXVerticalFrame.new(tab_book, LAYOUT_FILL)
-    FXLabel.new(tab1_frame, "Student_list_view", nil, LAYOUT_CENTER_X)
-    table_frame = FXHorizontalFrame.new(tab1_frame, LAYOUT_FILL)
-    @table = FXTable.new(table_frame, nil, 0, TABLE_COL_SIZABLE | LAYOUT_FILL)
-    @table.setTableSize(0, 4)
-    @table.setColumnText(0, "№")
-    @table.setColumnText(1, "ФИО")
-    @table.setColumnText(2, "Git")
-    @table.setColumnText(3, "Контакт")
-    tab2 = FXTabItem.new(tab_book, "2")
-    FXVerticalFrame.new(tab_book, LAYOUT_FILL).tap do |frame|
-      FXLabel.new(frame, "2", nil, LAYOUT_CENTER_X)
-    end
-    tab3 = FXTabItem.new(tab_book, "3")
-    FXVerticalFrame.new(tab_book, LAYOUT_FILL).tap do |frame|
-      FXLabel.new(frame, "3", nil, LAYOUT_CENTER_X)
-    end
-    quit_button = FXButton.new(self, "Закрыть окно", nil, nil, 0, FRAME_RAISED | LAYOUT_FILL_X)
-    quit_button.connect(SEL_COMMAND) { getApp().exit }
-  end
-  def create
-    super
-    show(PLACEMENT_SCREEN)
-  end
+include Fox
+
+class StudentsApp < FXMainWindow
+	def initialize(app)
+		super(app, "Students", :width => 1024, :height => 768)
+
+		sections = FXTabBook.new(self, opts: LAYOUT_FILL)
+
+		# ==1==
+		section1 = FXTabItem.new(sections, "Список студентов")
+		student_list_view = FXVerticalFrame.new(sections, opts: LAYOUT_FILL)
+
+		# filter
+		filter_frame = FXGroupBox.new(student_list_view, "Фильтрация", opts: GROUPBOX_NORMAL | LAYOUT_FILL_X)
+		FXLabel.new(filter_frame, "ФИО:")
+		fio_field = FXTextField.new(filter_frame, 25)
+
+		%w[Git Почта Телефон Telegram].each do |field_name|
+			field_frame = FXHorizontalFrame.new(filter_frame, opts: LAYOUT_FILL_X)
+			FXLabel.new(field_frame, "#{field_name}:")
+	      	choice = FXComboBox.new(field_frame, 15, opts: COMBOBOX_STATIC | FRAME_SUNKEN | FRAME_THICK)
+	      	choice.numVisible = 3
+	      	choice.appendItem("Да")
+	      	choice.appendItem("Нет")
+	      	choice.appendItem("Не важно")
+	      	field = FXTextField.new(field_frame, 25, opts: TEXTFIELD_NORMAL | TEXTFIELD_READONLY)
+
+	      	# enable/disable fields
+	      	choice.connect(SEL_COMMAND) do
+	        	field.enabled = choice.currentItem == 0
+	      	end
+	    end
+
+	    # students table
+	    table_frame = FXGroupBox.new(student_list_view, "Список студентов", opts: GROUPBOX_NORMAL | LAYOUT_FILL)
+	    student_table = FXTable.new(table_frame, opts: LAYOUT_FILL)
+	    student_table.setTableSize(5, 5) # rows, columns
+	    student_table.setColumnText(0, "ФИО")
+	    student_table.setColumnText(1, "Git")
+	    student_table.setColumnText(2, "Email")
+	    student_table.setColumnText(3, "Телефон")
+	    student_table.setColumnText(4, "Telegram")
+	    student_table.editable = false
+
+	   	# buttons
+	    control_frame = FXHorizontalFrame.new(student_list_view, opts: LAYOUT_FILL_X)
+	    FXButton.new(control_frame, "Добавить", nil, nil, 0, opts: BUTTON_NORMAL).connect(SEL_COMMAND) do
+	      	FXMessageBox.information(self, MBOX_OK, "Добавить", "Студент добавлен")
+	    end
+
+	    FXButton.new(control_frame, "Удалить", nil, nil, 0, opts: BUTTON_NORMAL).connect(SEL_COMMAND) do
+	      	FXMessageBox.warning(self, MBOX_OK, "Удалить", "Студент удален")
+	    end
+
+	    # ==2==
+	    section2 = FXTabItem.new(sections, "2")
+	    FXVerticalFrame.new(sections, opts: LAYOUT_FILL) { FXLabel.new(self, "content 2") }
+
+	    # ==3==
+	    section3 = FXTabItem.new(sections, "3")
+	    FXVerticalFrame.new(sections, opts: LAYOUT_FILL) { FXLabel.new(self, "content 3") }
+	end
+
+	def create
+		super
+		show(PLACEMENT_SCREEN)
+	end
+end
+
+if __FILE__ == $0
+	app = FXApp.new
+	StudentsApp.new(app)
+	app.create
+	app.run
 end
